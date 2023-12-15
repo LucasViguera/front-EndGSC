b9bfdf818dda4d8590c9d9d02a41cfff03d55bf3
Lucas Viguera <123002129+LucasViguera@users.noreply.github.com>
2023-12-14 23:11:59 -0300

    quitar comentarios

--- Angular/suscripcion-revista/src/app/subscripcion.service.ts
+++ Angular/suscripcion-revista/src/app/subscripcion.service.ts
@@ -6,6 +6,5 @@ import { Injectable } from '@angular/core';
 export class SubscripcionService {
   registrarSuscripcion(datosSuscripcion: any) {
     console.log('Datos de la suscripción:', datosSuscripcion);
-    // Aquí puedes realizar acciones con los datos de la suscripción, como enviar a un servidor.
   }
 }
